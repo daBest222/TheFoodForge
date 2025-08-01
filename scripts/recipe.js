82b2cdef6608eec32563939885c2968680a11d13
daBest222 <barcellidorian2@gmail.com>
2025-08-01 16:07:20 +1200

    .

--- scripts/recipe.js
+++ scripts/recipe.js
@@ -1,5 +1,15 @@
 function displayRecipe(recipe)
 {
-    document.getElementById("recipeName").innerHTML = recipe.name;
+    document.getElementsByClassName("recipeName").array.forEach(element => 
+    {
+        element.innerHTML = recipe.name;
+    });
     document.getElementById("recipeDescription").innerHTML = recipe.description;
+    document.getElementById("recipeImage").src = recipe.image;
+    document.getElementById("recipeTime").innerHTML += recipe.time;
+    document.getElementById("recipeCategory").innerHTML += recipe.category;
+
+    var favouriteButton = document.getElementById("favouriteButton");
+    favouriteButton.id = recipe.id;
+    setFavouriteButton(favouriteButton, recipe.id);
 }
